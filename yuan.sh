c40a684f293b786c3e408954bc384ae12414dfa6
qianmianyao <32097980+zp1998421@users.noreply.github.com>
2019-04-19 09:47:01 +0800

    centos7换源脚本

---
+++ yuan.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+centos(){
+	if [ $(id -u) == 0 ];then
+		start
+	else
+		echo -e "请使用centos7"
+		exit 2
+	fi
+}
+yum install -y wget
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+yum_epel(){
+	yum install -y epel*
+}
+
+yum_ali(){
+	wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+	yum makecache
+}
+
+yum_163(){
+	wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS7-Base-163.repo
+	yum makecache
+}
+
+yum_tuna(){
+	wget -O /etc/yum.repos.d/CentOS-Base.repo https://raw.githubusercontent.com/zp1998421/shell/master/yuan/tuna
+	yum makecache
+}
+
+start(){
+	echo -e "${green}1.安装epel源${fond}"
+	echo -e "${green}2.安装阿里源${fond}"
+	echo -e "${green}3.安装163源${fond}"
+	echo -e "${green}4.安装清华源${fond}"
+	read -p "选择[1,2,3,4]"yum_install
+	case $yum_install in
+		1)
+		yum_epel
+		;;
+		2)
+		yum_ali
+		;;
+		3)
+		yum_163
+		;;
+		4)
+		yum_tuna
+		;;
+		*)
+		mv /etc/yum.repos.d/CentOS-Base.repo.backup /etc/yum.repos.d/CentOS-Base.repo
+		exit 1
+		;;
+	esac
+}
+
+root
