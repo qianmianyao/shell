fa1e1e8e0e56d70ec01ac0e38ea4dbc5c71cbec2
qianmianyao <32097980+qianmianyao@users.noreply.github.com>
2021-01-02 16:34:55 +0800

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 apt install remove docker docker-engine docker.io containerd runc
 apt-get update
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common \
-    sudo
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common sudo
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
